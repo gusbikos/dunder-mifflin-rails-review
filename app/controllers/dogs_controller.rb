552fae28deafb8e79a6ae9e0b76ecdf43e1ed04c
gusbikos <gusbikos@gmail.com>
2021-03-03 20:36:58 -0500

    commit tree 1c21b7df43ea793914c14ccc889c200dc6593072

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,17 @@
 class DogsController < ApplicationController
+
+    def index 
+        @dogs = Dog.all
+    end
+
+    def show 
+        @dog = Dog.find(params[:id])
+        @employees = @dog.employees
+    end 
+
+    def sort_dogs
+        @sort = Dog.all.sort_by {|e| e.employees.size}.reverse
+    end 
+
+  
 end
